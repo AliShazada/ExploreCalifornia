5b0eb7755a1177e82d49e80c81bf87115057c335
Ali Ahmad Shazada <aliahmadshahzada@outlook.com>
2019-07-24 18:54:47 +1000

    Middleware

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -20,12 +20,24 @@ namespace ExploreCalifornia
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            app.UseExceptionHandler("/error.html");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
 
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                {
+                    throw new Exception("ERROR!");
+                }
+                await next();
 
+            });
 
             //app.Use(async (context,next) => // This is the only middleware regisererd
             //{
